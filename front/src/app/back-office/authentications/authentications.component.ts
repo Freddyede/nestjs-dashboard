8bde44190faedf076fbbf1917fd3c98b3206aff4
Franck <136079691+Freddyede@users.noreply.github.com>
2025-03-22 18:44:54 +0100

    Finish adding UI for create new session

--- front/src/app/back-office/authentications/authentications.component.ts
+++ front/src/app/back-office/authentications/authentications.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IUser } from '../../users';
 import {RouterLink} from '@angular/router';
+import {UserFormService} from '../../user-form.service';
 
 @Component({
   selector: 'app-authentications',
@@ -10,6 +11,13 @@ import {RouterLink} from '@angular/router';
   templateUrl: './authentications.component.html',
   styleUrl: './authentications.component.less'
 })
-export class AuthenticationsComponent {
+export class AuthenticationsComponent implements OnInit {
   users: IUser[] | undefined = [];
+  constructor(public userFormService: UserFormService) {
+  }
+  ngOnInit() {
+    this.userFormService.getUsers().subscribe((res: any) => {
+      console.log(res);
+    });
+  }
 }
--- front/src/app/user-form.service.ts
+++ front/src/app/user-form.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {User} from './user';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,14 @@ export class UserFormService {
   user: FormGroup;
   constructor(
     protected fb:FormBuilder,
+    private router: Router,
     private http: HttpClient
   ) {
 
     this.user = this.fb.group({
       email: ['',Validators.required],
       password: ['',Validators.required],
-      roleName: ['',Validators.required]
+      roleName: ['',Validators.required],
     });
   }
 
@@ -29,8 +31,6 @@ export class UserFormService {
     const tokenStorage = localStorage.getItem('token');
     const userStorage = localStorage.getItem('user');
     const userStorageObject = userStorage ? JSON.parse(userStorage) : undefined;
-    if (tokenStorage && userStorage) {
-    }
     console.log(this.user.value);
     let headers = new HttpHeaders({
       'Authorization': `Bearer ${tokenStorage}`,
@@ -38,7 +38,20 @@ export class UserFormService {
       'Content-Type': 'application/json'
     });
     this.http.post<User>('http://localhost:3000/dashboard/auth/register', this.user.value, {headers: headers}).subscribe((res: any) => {
-      console.log(res);
+      if (res.status === 201 && res.message === 'User registered successfully!') {
+        this.router.navigateByUrl('dashboard/authentications/list').then();
+      }
     })
+  }
+  getUsers()  {
+    const tokenStorage = localStorage.getItem('token');
+    const userStorage = localStorage.getItem('user');
+    const userStorageObject = userStorage ? JSON.parse(userStorage) : undefined;
+    let headers = new HttpHeaders({
+      'Authorization': `Bearer ${tokenStorage}`,
+      'Access': userStorageObject.roles.access_token,
+      'Content-Type': 'application/json'
+    });
+    return this.http.get('http://localhost:3000/users/list', {headers: headers});
   }
 }
