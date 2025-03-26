7e6c2f97cf3acd8ea103b1af9ed0bdefecb261e2
Franck <136079691+Freddyede@users.noreply.github.com>
2025-03-26 11:02:52 +0100

    Modify AuthGuard by AccessGuard

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { UserRepository } from '../database/repository/user.repository';
 import { DatabaseModule } from '../database/database.module';
 import { RoleRepository } from '../database/repository/role.repository';
 import { AuthGuard } from '../guards/auth.guard';
-import {AccessGuard} from "../guards/access.guard";
+import { AccessGuard } from '../guards/access.guard';
 
 @Module({
   imports: [
