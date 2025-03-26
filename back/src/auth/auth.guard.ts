e788f2a07622ac2ee142727f2d600ada8621980d
Franck <136079691+Freddyede@users.noreply.github.com>
2025-03-26 08:42:25 +0100

    Modify Authentification by access headers key

--- back/src/auth/auth.guard.ts
+++ back/src/auth/auth.guard.ts
@@ -14,12 +14,33 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
     const access: boolean = await this.extractTokenAccessFromHeader(request);
-    if (!access) {
-      throw new UnauthorizedException();
-    } else {
+    if(token !== undefined) {
+      if (!token && !access) {
+        throw new UnauthorizedException();
+      }
+      try {
+        // 💡 We're assigning the payload to the request object here
+        // so that we can access it in our route handlers
+        request['user'] = await this.jwtService.verifyAsync(token, {
+          secret: process.env.JWT_SECRET,
+        });
+      } catch {
+        throw new UnauthorizedException();
+      }
       return true;
+    } else {
+      if (!access) {
+        throw new UnauthorizedException();
+      } else {
+        return true;
+      }
     }
+  }
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
   private async extractTokenAccessFromHeader(
     request: Request,
